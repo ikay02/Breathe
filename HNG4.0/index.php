c30a00cb806fbbfb3ee185504238ad9761477958
Nnaji Michael <37900520+ikay02@users.noreply.github.com>
2018-04-14 12:23:59 +0100

    Add files via upload

---
+++ HNG4.0/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Stage 1 #HNG4</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+
+    <div class="header">
+        <header>
+            #HNG4.0
+        </header>
+    </div>
+    <div class="main">
+HOLA!
+<h1>echo date('h:i:s');</h1>
+    </div>
+    <div class="footer">
+        <footer>
+ciao!
+        </footer>
+    </div>
+
+</body>
+</html>
